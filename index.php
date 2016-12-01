70295aba97e74edf5f31cab609eb95a2dafa14f8
Sandro Caputo <sandrolomo@gmail.com>
2016-12-01 02:32:38 +0100

    Works?
    Buggy as fudge but mostly works.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 	<link rel="stylesheet" href="assets/style/custom.css" type="text/css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -19,29 +20,60 @@
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-	mysqli_select_db($conn, "scheduler");
+	mysqli_select_db($conn, "testie");
 
 	?>
-
 
 <div id="overlay"></div>
 <form action="index.php" method="post">
 		<input type="text" id="datepicker" method="post" name="date">
-		<input id="timepicker" type="text" class="time ui-timepicker-input" autocomplete="off" method="post" name="time">
+		<select method="post" name="time" id="dropdown">
+			<option>1</option value="1">
+			<option>2</option value="2">
+			<option>3</option value="3">
+			<option>4</option value="4">
+			<option>5</option value="5">
+			<option>6</option value="6">
+		</select>
+		<input type="text" id="datepicker" method="post" name="name">
 		<p><input type="submit" /></p>
 
+<?php
+$taken_times = mysqli_query($conn, "SELECT time FROM scheduler");
+$rows = mysqli_fetch_all($taken_times);
+?>
+<script>
+var arr = '<?php echo json_encode($rows); ?>';
+num_of_dropped_kids = document.getElementById("dropdown").childElementCount;
+elements = document.getElementsByTagName("option");
+
+for (i = 0; i < num_of_dropped_kids; i++){
+	value = elements[i].value;
+	contains_bool = jQuery.inArray(value, arr);
+	console.log(value);
+	console.log(contains_bool);
+	if (contains_bool !== -1) {
+		elements[i].setAttribute("style", "background-color: red;");
+		elements[i].setAttribute("disabled", "");
+	};
+};
+</script>
+
 		<?php
-				$date = htmlspecialchars($_POST['date']);
-				$time = htmlspecialchars($_POST['time']);
+				$date = $_POST['date'];
+				$time = $_POST['time'];
+				$name = htmlspecialchars($_POST['name']);
 
-				$test = mysqli_query($conn, "INSERT INTO scheduler (date, time) VALUES ('$date', '$time')");
+				$test = mysqli_query($conn, "INSERT INTO scheduler (date, time, name) VALUES ('$date', '$time', '$name')");
+				$one = mysqli_error($conn);
 
-				$testie = mysqli_query($conn, "SELECT date, time FROM scheduler WHERE time = '11:00'");
+				$testie = mysqli_query($conn, "SELECT date, time, name FROM scheduler WHERE time = '1'");
 				$rowie = mysqli_fetch_row($testie);
 		 ?>
 </form>
+
 <p>
-	<?php echo $rowie[0]; echo $rowie[1]; ?>
+	<?php echo $one; ?>
 </p>
 
 
